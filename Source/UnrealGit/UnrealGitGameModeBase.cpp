41b6719a5253fcdb64ec12139350c52c583c88b3
Macsmith <albertocalderonmunoz@gmail.com>
2022-11-30 18:23:33 +0100

    Repair commit

--- Source/UnrealGit/UnrealGitGameModeBase.cpp
+++ Source/UnrealGit/UnrealGitGameModeBase.cpp
@@ -6,12 +6,9 @@
 AUnrealGitGameModeBase::AUnrealGitGameModeBase()
 	:Super()
 {
-<<<<<<< HEAD
 
 
 	UE_LOG(LogTemp, Warning, TEXT("Hola soy Alberto"));
 	
-=======
 	UE_LOG(LogTemp, Warning, TEXT("Hola soy Manu"));
->>>>>>> d6b1dd581745f3c5d0c1d5edbac82b9fe85e9f55
 }
