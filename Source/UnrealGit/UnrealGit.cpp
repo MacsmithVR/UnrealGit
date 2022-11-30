63d44cc681b39f72a0df435cbc0111d60bf78375
Macsmith <albertocalderonmunoz@gmail.com>
2022-11-30 17:46:31 +0100

    Initial commit

---
+++ Source/UnrealGit/UnrealGit.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealGit.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealGit, "UnrealGit" );
---
+++ Source/UnrealGit/UnrealGitGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealGitGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALGIT_API AUnrealGitGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
